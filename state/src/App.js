7d70cce5b636fee75daa549d48eda291672526a8
AlaaDridii <127346208+AlaaDridii@users.noreply.github.com>
2023-06-07 15:59:47 +0100

    Update App.js

--- state/src/App.js
+++ state/src/App.js
@@ -47,19 +47,19 @@ export default class App extends Component {
       <Card.Img className="avion" variant="top" src={this.state.imgg} />    {/*Appelation de notre State img*/}
       <Card.Body>
         <div>
-        <h2>Ajouter à la Flotte Aérienne:{this.state.compter}</h2>          {/*Appelation de notre state du compteur*/}
+        <h2>{this.state.compter}</h2>          {/*Appelation de notre state du compteur*/}
         <Button variant="primary" onClick={this.ascendant}>+</Button>{'  '} {/*Créer un boutton où on met une action (au clic:incremente la valeur de compteur par 1)*/}
         <Button variant="danger" onClick={this.descendant}>-</Button>{' '}  {/*Créer un boutton où on met une action (au clic:déccremente la valeur de compteur par 1)*/}
         </div>
         <hr/>
 
-        <Card.Title><h3>{this.state.name}</h3></Card.Title>                { /*Appelation de notre state Nom de l'aéronef*/}
+        <Card.Title><h3>{this.state.name}</h3></Card.Title>                { /*Appelation de notre state Nom du personne/}
         <Card.Text>
           {/*Condition: si this.state.show=0 ET Texte.js=1 alors la description va etre masquer càd=0
                      : si nn si this.state.show=1 Et Texte.js=1(toujours active) alors la desc va etre afficher càd=1*/}
           {this.state.show && <Texte/>}                                     
         </Card.Text>
-        <Button variant="primary" onClick={this.toggle}>Afficher/Masquer les détails</Button> {/*Créer un boutton où on met une action (au clic:Changer la viibilité de la valeur de la description que l'on a importé depuis Texte.js)*/}
+        <Button variant="primary" onClick={this.toggle}>Afficher/Masquer BIO</Button> {/*Créer un boutton où on met une action (au clic:Changer la viibilité de la valeur de la description que l'on a importé depuis Texte.js)*/}
        
       </Card.Body>
     </Card>
